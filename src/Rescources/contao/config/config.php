38f617e07468d705faa0a7a65cf68b1348f3d242
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2019-08-30 15:43:57 +0200

    Create config.php

---
+++ src/Rescources/contao/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Back end modules
+ */
+
+array_insert($GLOBALS['BE_MOD']['calendar_booking'], sizeof($GLOBALS['BE_MOD']['calendar_booking']), array('lb_costtype' => array
+    (
+        'tables'        => array('tl_lb_costType')
+    )
+));
+
+if(\Input::get('do') == 'calendar'&&\Input::get('id')==5)
+{
+   file_put_contents("calendar","");
+   $GLOBALS['TL_JAVASCRIPT'][] = '/bundles/extendedproductdetail/js/backend.js';
+   $GLOBALS['TL_CSS'][] = '/bundles/extendedproductdetail/js/backend.css';
+}  
